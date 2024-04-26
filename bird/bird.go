c5825f4b319d164b6f8eb251e38a3b5b6373d515
BaoCaiH <caihongbao280996@gmail.com>
2024-04-26 17:36:28 +0300

    Drop overengineered Vec2

--- bird/bird.go
+++ bird/bird.go
@@ -7,23 +7,24 @@ import (
 )
 
 type Bird struct {
-	Pos    utils.Vec2
-	Hitbox utils.Vec2
+	X, Y, HX, HY int
+	// Magnitude of "gravity" relative to the screen, 1 means move down 1 line
+	Moving int
 }
 
 func Init(startX, startY int) *Bird {
-	return &Bird{Pos: utils.Vec2{X: startX, Y: startY}, Hitbox: utils.Vec2{X: 3, Y: 2}}
+	return &Bird{X: startX, Y: startY, HX: 3, HY: 2, Moving: 1}
 }
 
 func (b *Bird) Render(x, y int, bg termbox.Attribute) {
-	top := y + b.Pos.Y
-	left := x + b.Pos.X
+	top := y + b.Y
+	left := x + b.X
 
 	// First line
-	utils.Draw(left, top, "◢■", termbox.ColorLightRed, bg)
+	utils.Draw(left, top, "◢■", termbox.ColorYellow, bg)
 	termbox.SetCell(left+2, top, 'Ꙩ', termbox.ColorWhite, bg)
 	// Second line
 	termbox.SetCell(left, top+1, '◀', termbox.ColorWhite, bg)
-	utils.Draw(left+1, top+1, "■■", termbox.ColorLightRed, bg)
-	termbox.SetCell(left+3, top+1, '=', termbox.ColorYellow, bg)
+	utils.Draw(left+1, top+1, "■■", termbox.ColorYellow, bg)
+	termbox.SetCell(left+3, top+1, '=', termbox.ColorLightRed, bg)
 }
--- utils/vec.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-type Vec2 struct {
-	X, Y int
-}
-
-func (v *Vec2) Add(w *Vec2) Vec2 {
-	return Vec2{v.X + w.X, v.Y + w.Y}
-}
