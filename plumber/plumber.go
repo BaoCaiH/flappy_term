ff90c6f60396d21286fdd15c06b273ba147f1d65
BaoCaiH <46781181+BaoCaiH@users.noreply.github.com>
2024-04-27 11:41:16 +0300

    Refactor plumber to work on any screensize

--- plumber/plumber.go
+++ plumber/plumber.go
@@ -13,53 +13,39 @@ type Pipe struct {
 }
 
 const (
-	pipeLength = 20
-	pipeGap    = 7
-	pipeWidth  = 5
-	pipeDist   = 11
+	PipeGap   = 7
+	PipeWidth = 5
+	pipeDist  = 11
 )
 
-//	func Init(startX, startY int) *Pipe {
-//		return &Pipe{X: startX, Y: startY}
-//	}
 func Init() []*Pipe {
 	return []*Pipe{
-		{42, rand.IntN(12) - 12},
-		{42 + pipeWidth + pipeDist, rand.IntN(12) - 12},
-		{42 + (pipeWidth+pipeDist)*2, rand.IntN(12) - 12},
+		{42, rand.IntN(24-6-PipeGap) + 3},
+		{42 + PipeWidth + pipeDist, rand.IntN(24-6-PipeGap) + 3},
+		{42 + (PipeWidth+pipeDist)*2, rand.IntN(24-6-PipeGap) + 3},
 	}
 }
 
 func (p *Pipe) Render(top, bottom, left, right int, bg termbox.Attribute) {
 	pipeLeft := p.X + left
-	pipeRight := p.X + left + pipeWidth
-	showableWidth := pipeWidth
+	pipeRight := p.X + left + PipeWidth
+	showableWidth := PipeWidth
 	if pipeLeft < left {
 		showableWidth -= min(left-pipeLeft, showableWidth)
 	} else if pipeRight > right-1 {
 		showableWidth -= min(pipeRight-right+1, showableWidth)
 	}
-	for i := 0; i < 20; i++ {
-		if i == 19 {
-			utils.Draw(max(pipeLeft, left), p.Y+i, strings.Repeat("#", showableWidth), termbox.ColorGreen, bg)
-		} else if i == 0 {
-			utils.Draw(max(pipeLeft, left), p.Y+pipeLength+pipeGap, strings.Repeat("#", showableWidth), termbox.ColorGreen, bg)
-		} else {
-			if p.Y+i > top {
-				if pipeLeft >= left && pipeLeft < right-1 {
-					termbox.SetCell(pipeLeft, p.Y+i, '#', termbox.ColorGreen, bg)
-				}
-				if pipeLeft+pipeWidth-1 >= left && pipeLeft+pipeWidth-1 < right-1 {
-					termbox.SetCell(pipeLeft+pipeWidth-1, p.Y+i, '#', termbox.ColorGreen, bg)
-				}
+	for i := top + 1; i < bottom; i++ {
+		if i == p.Y+top {
+			utils.Draw(max(pipeLeft, left), i, strings.Repeat("#", showableWidth), termbox.ColorGreen, bg)
+		} else if i == p.Y+top+PipeGap+1 {
+			utils.Draw(max(pipeLeft, left), i, strings.Repeat("#", showableWidth), termbox.ColorGreen, bg)
+		} else if i < p.Y+top || i > p.Y+top+PipeGap+1 {
+			if pipeLeft >= left && pipeLeft < right-1 {
+				termbox.SetCell(pipeLeft, i, '#', termbox.ColorGreen, bg)
 			}
-			if p.Y+i+pipeLength+pipeGap < bottom {
-				if pipeLeft >= left && pipeLeft < right-1 {
-					termbox.SetCell(pipeLeft, p.Y+i+pipeLength+pipeGap, '#', termbox.ColorGreen, bg)
-				}
-				if pipeLeft+pipeWidth-1 >= left && pipeLeft+pipeWidth-1 < right-1 {
-					termbox.SetCell(pipeLeft+pipeWidth-1, p.Y+i+pipeLength+pipeGap, '#', termbox.ColorGreen, bg)
-				}
+			if pipeLeft+PipeWidth-1 >= left && pipeLeft+PipeWidth-1 < right-1 {
+				termbox.SetCell(pipeLeft+PipeWidth-1, i, '#', termbox.ColorGreen, bg)
 			}
 		}
 	}
@@ -67,11 +53,11 @@ func (p *Pipe) Render(top, bottom, left, right int, bg termbox.Attribute) {
 
 func Reset(pipes []*Pipe) {
 	for i, p := range pipes {
-		p.X = 42 + i*(pipeWidth+pipeDist)
+		p.X = 42 + i*(PipeWidth+pipeDist)
 	}
 }
 
 func (p *Pipe) Reset() {
-	p.X = pipeWidth*2 + pipeDist*3
-	p.Y = rand.IntN(12) - 12
+	p.X = PipeWidth*2 + pipeDist*3
+	p.Y = rand.IntN(24-6-PipeGap) + 3
 }
